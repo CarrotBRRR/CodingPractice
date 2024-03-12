8cdbe45fb149082e4f2085e0a8137fa61a42da47
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-03-11 20:53:00 -0600

    Fixed canJump

--- canJump.cpp
+++ canJump.cpp
@@ -4,20 +4,14 @@ using namespace std;
 class Solution {
     public:
         bool canJump(vector<int>& nums) {
-            int i = 0;
-            while(i < nums.size()){
-                if(nums[i] == 0){
-                    break;
+            int n = nums.size();
+            int maxReach = 0;
+            for(int i = 0; i < n; i++) {
+                if(i > maxReach) {
+                    return false;
                 }
-
-                i += nums[i];
+                maxReach = max(maxReach, i + nums[i]);
             }
-
-            int n = nums.size() + nums.end() - 1
-            if(i == n){
-                return true;
-            } else {
-                return false;
-            }
+            return true;
         }
 };
